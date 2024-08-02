db5f835e7f9db679a4855a9ce152c547f436f89a
Vraj1103 <svraj073@gmail.com>
2024-08-02 16:58:50 +0530

    doneee

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const app = express();
 app.use(express.json());
 
 connectDb();
+app.use("/api", emailRoutes);
 app.get("/", (req, res) => {
   res.send("Email Schdeuler by Vraj!");
 });
-app.use("/api", emailRoutes);
 
 app.use(errorHandler);
 
